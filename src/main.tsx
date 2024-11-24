6f60a11e4cf0aac84ca06f00901dbe5e8c70c6df
Marco Arduini <mar.arduini@gmail.com>
2024-11-24 14:53:24 +0100

    Install Tailwind CSS
    Easy way to manage CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./build.css";
 
 if (import.meta.env.DEV) {
   loadDevMessages();
@@ -11,14 +11,14 @@ if (import.meta.env.DEV) {
 }
 
 const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com/graphql',
+  uri: "https://countries.trevorblades.com/graphql",
   cache: new InMemoryCache(),
 });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
