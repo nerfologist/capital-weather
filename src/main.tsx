3dcd094b96d34f36cc7a9a28ba4eb428e6242ed7
Marco Arduini <mar.arduini@gmail.com>
2024-11-24 14:47:20 +0100

    Load error and dev messages from Apollo in dev mode
    Make it easier to debug problems

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 import App from './App.tsx'
 import './index.css'
+
+if (import.meta.env.DEV) {
+  loadDevMessages();
+  loadErrorMessages();
+}
 
 const client = new ApolloClient({
   uri: 'https://countries.trevorblades.com/graphql',
