99cf6a958c5293c9d75ca795670f66c816fa076c
Marco Arduini <mar.arduini@gmail.com>
2024-11-25 10:44:12 +0100

    Set up simple sidebar and main panel design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@apollo/client";
-import { GET_COUNTRIES } from './graphql/queries'
+import { GET_COUNTRIES } from "./graphql/queries";
 
 type Country = {
-  name: string
-  code: string
-}
+  name: string;
+  code: string;
+};
 
 function App() {
   const { loading, error, data } = useQuery(GET_COUNTRIES);
@@ -13,12 +13,15 @@ function App() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="App">
-      <ul>
-        {data.countries.map((country: Country) => (
-          <li key={country.code}>{country.name}</li>
-        ))}
-      </ul>
+    <div className="App relative min-h-screen flex">
+      <aside className="bg-blue-800 text-blue-100 w-64 p-10">
+        <ul>
+          {data.countries.map((country: Country) => (
+            <li key={country.code}>{country.name}</li>
+          ))}
+        </ul>
+      </aside>
+      <main className="p-10 flex-1">main content</main>
     </div>
   );
 }
